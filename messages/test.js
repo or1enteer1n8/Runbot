b98a3a49ae279f0bfc8bce792e239ac96f1e2e0b
or1enteer1n8 <or1enteer1n8@gmail.com>
2017-06-14 08:55:24 +0900

    cheerio-httpcli testing...

--- messages/test.js
+++ messages/test.js
@@ -1,12 +1,14 @@
 'use strict'
 
-var client = require('cheerio-httpcli');
+let client = require('cheerio-httpcli');
 const URL = 'https://runnet.jp/entry/runtes/user/pc/competitionDetailAction.do?raceId=156693&div=1&tabFlg=4#tab'
 
+let array = [];
+let count = 0;
 client.fetch(URL)
-.then(function(result){
-    result.$('td.entryArea02').each(function(index,element){
-        console.log(element.childNodes);
-    });
-})
+    .then(function(result){
+        var $ = result.$;
+        console.log($('.topTbl').attr('class'));
+        console(count++);
+    })
 
